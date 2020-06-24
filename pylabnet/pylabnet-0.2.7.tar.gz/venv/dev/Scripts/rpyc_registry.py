40fdca8dd8766cb31ec79e6db20f4ca4dc65b60d
Tom Forbes <tom@tomforb.es>
2020-06-24 16:58:57 +0000

    {"name":"pylabnet","version":"0.2.7","file":"pylabnet-0.2.7.tar.gz","path":"pylabnet/pylabnet-0.2.7.tar.gz"}

---
+++ pylabnet/pylabnet-0.2.7.tar.gz/venv/dev/Scripts/rpyc_registry.py
@@ -0,0 +1,4 @@
+#!C:\Users\mbhas\pylabnet\venv\dev\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'rpyc==4.1.5','rpyc_registry.py'
+__requires__ = 'rpyc==4.1.5'
+__import__('pkg_resources').run_script('rpyc==4.1.5', 'rpyc_registry.py')
